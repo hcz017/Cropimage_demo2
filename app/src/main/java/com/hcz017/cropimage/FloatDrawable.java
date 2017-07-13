266666bbe5dc38204b4214b8602eb7370f5f4394
hcz017 <hcz017@gmail.com>
2017-07-14 00:59:20 +0800

    optimize

--- app/src/main/java/com/hcz017/cropimage/FloatDrawable.java
+++ app/src/main/java/com/hcz017/cropimage/FloatDrawable.java
@@ -1,11 +1,11 @@
 package com.hcz017.cropimage;
 
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 
@@ -37,11 +37,11 @@ public class FloatDrawable extends Drawable {
     }
 
     public int getBorderWidth() {
-        return dipTopx(mContext, offset);//根据dip计算的像素值，做适配用的
+        return dipToPx(mContext, offset);//根据dip计算的像素值，做适配用的
     }
 
     public int getBorderHeight() {
-        return dipTopx(mContext, offset);
+        return dipToPx(mContext, offset);
     }
 
     @Override
@@ -52,54 +52,53 @@ public class FloatDrawable extends Drawable {
         int right = getBounds().right;
         int bottom = getBounds().bottom;
 
-        Rect mRect = new Rect(left + dipTopx(mContext, offset) / 2, top
-                + dipTopx(mContext, offset) / 2, right
-                - dipTopx(mContext, offset) / 2, bottom
-                - dipTopx(mContext, offset) / 2);
+        Rect mRect = new Rect(left + dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2,
+                right - dipToPx(mContext, offset) / 2,
+                bottom - dipToPx(mContext, offset) / 2);
         //画默认的选择框
         canvas.drawRect(mRect, mLinePaint);
         //画四个角的四个粗拐角、也就是八条粗线
-        canvas.drawLine((left + dipTopx(mContext, offset) / 2 - 3.5f), top
-                        + dipTopx(mContext, offset) / 2,
-                left + dipTopx(mContext, offset) - 8f,
-                top + dipTopx(mContext, offset) / 2, mLinePaint2);
-        canvas.drawLine(left + dipTopx(mContext, offset) / 2,
-                top + dipTopx(mContext, offset) / 2,
-                left + dipTopx(mContext, offset) / 2,
-                top + dipTopx(mContext, offset) / 2 + 30, mLinePaint2);
-        canvas.drawLine(right - dipTopx(mContext, offset) + 8f,
-                top + dipTopx(mContext, offset) / 2,
-                right - dipTopx(mContext, offset) / 2,
-                top + dipTopx(mContext, offset) / 2, mLinePaint2);
-        canvas.drawLine(right - dipTopx(mContext, offset) / 2,
-                top + dipTopx(mContext, offset) / 2 - 3.5f,
-                right - dipTopx(mContext, offset) / 2,
-                top + dipTopx(mContext, offset) / 2 + 30, mLinePaint2);
-        canvas.drawLine((left + dipTopx(mContext, offset) / 2 - 3.5f), bottom
-                        - dipTopx(mContext, offset) / 2,
-                left + dipTopx(mContext, offset) - 8f,
-                bottom - dipTopx(mContext, offset) / 2, mLinePaint2);
-        canvas.drawLine((left + dipTopx(mContext, offset) / 2), bottom
-                        - dipTopx(mContext, offset) / 2,
-                (left + dipTopx(mContext, offset) / 2),
-                bottom - dipTopx(mContext, offset) / 2 - 30f, mLinePaint2);
-        canvas.drawLine((right - dipTopx(mContext, offset) + 8f), bottom
-                        - dipTopx(mContext, offset) / 2,
-                right - dipTopx(mContext, offset) / 2,
-                bottom - dipTopx(mContext, offset) / 2, mLinePaint2);
-        canvas.drawLine((right - dipTopx(mContext, offset) / 2), bottom
-                        - dipTopx(mContext, offset) / 2 - 30f,
-                right - dipTopx(mContext, offset) / 2,
-                bottom - dipTopx(mContext, offset) / 2 + 3.5f, mLinePaint2);
-
+        canvas.drawLine((left + dipToPx(mContext, offset) / 2 - 3.5f),
+                top + dipToPx(mContext, offset) / 2,
+                left + dipToPx(mContext, offset) - 8f,
+                top + dipToPx(mContext, offset) / 2, mLinePaint2);
+        canvas.drawLine(left + dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2,
+                left + dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2 + 30, mLinePaint2);
+        canvas.drawLine(right - dipToPx(mContext, offset) + 8f,
+                top + dipToPx(mContext, offset) / 2,
+                right - dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2, mLinePaint2);
+        canvas.drawLine(right - dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2 - 3.5f,
+                right - dipToPx(mContext, offset) / 2,
+                top + dipToPx(mContext, offset) / 2 + 30, mLinePaint2);
+        canvas.drawLine((left + dipToPx(mContext, offset) / 2 - 3.5f),
+                bottom - dipToPx(mContext, offset) / 2,
+                left + dipToPx(mContext, offset) - 8f,
+                bottom - dipToPx(mContext, offset) / 2, mLinePaint2);
+        canvas.drawLine((left + dipToPx(mContext, offset) / 2),
+                bottom - dipToPx(mContext, offset) / 2,
+                left + dipToPx(mContext, offset) / 2,
+                bottom - dipToPx(mContext, offset) / 2 - 30f, mLinePaint2);
+        canvas.drawLine((right - dipToPx(mContext, offset) + 8f),
+                bottom - dipToPx(mContext, offset) / 2,
+                right - dipToPx(mContext, offset) / 2,
+                bottom - dipToPx(mContext, offset) / 2, mLinePaint2);
+        canvas.drawLine((right - dipToPx(mContext, offset) / 2),
+                bottom - dipToPx(mContext, offset) / 2 - 30f,
+                right - dipToPx(mContext, offset) / 2,
+                bottom - dipToPx(mContext, offset) / 2 + 3.5f, mLinePaint2);
     }
 
     @Override
     public void setBounds(Rect bounds) {
-        super.setBounds(new Rect(bounds.left - dipTopx(mContext, offset) / 2,
-                bounds.top - dipTopx(mContext, offset) / 2, bounds.right
-                + dipTopx(mContext, offset) / 2, bounds.bottom
-                + dipTopx(mContext, offset) / 2));
+        super.setBounds(new Rect(bounds.left - dipToPx(mContext, offset) / 2,
+                bounds.top - dipToPx(mContext, offset) / 2,
+                bounds.right + dipToPx(mContext, offset) / 2,
+                bounds.bottom + dipToPx(mContext, offset) / 2));
     }
 
     @Override
@@ -114,12 +113,11 @@ public class FloatDrawable extends Drawable {
 
     @Override
     public int getOpacity() {
-        return 0;
+        return PixelFormat.UNKNOWN;
     }
 
-    public int dipTopx(Context context, float dpValue) {
+    public int dipToPx(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
     }
-
 }
--- app/src/main/java/com/hcz017/cropimage/MainActivity.java
+++ app/src/main/java/com/hcz017/cropimage/MainActivity.java
@@ -1,19 +1,53 @@
 package com.hcz017.cropimage;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private CropImageView mView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CropImageView mCropImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mView = (CropImageView) findViewById(R.id.cropimage);
-        // 设置资源和默认长宽
-        mView.setDrawable(getResources().getDrawable(R.drawable.test2), 300, 300);
-        // 调用该方法得到剪裁好的图片
-        // Bitmap mBitmap= mView.getCropImage();
+        mCropImageView = (CropImageView) findViewById(R.id.cropimage);
+        Button mBtnImg1 = (Button) findViewById(R.id.btn_img1);
+        Button mBtnImg2 = (Button) findViewById(R.id.btn_img2);
+        Button mBtnCut = (Button) findViewById(R.id.btn_cut);
+        mBtnImg1.setOnClickListener(this);
+        mBtnImg2.setOnClickListener(this);
+        mBtnCut.setOnClickListener(this);
+
+        // 调用该方法得到剪裁好的图片，保存这个bitmap就可以保存裁剪后的图片
+        // Bitmap mBitmap= mCropImageView.getCropImage();
+    }
+
+    @Override
+    public void onClick(View v) {
+        Bitmap bitmap;
+        // 这里设置visibility为GONE 是为了触发CropImageView里面的onMeasure来重新设置控件大小
+        mCropImageView.setVisibility(View.GONE);
+        switch (v.getId()){
+            case R.id.btn_img1:
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
+                mCropImageView.setDrawable(bitmap, 200, 300);
+                mCropImageView.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_img2:
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.husun);
+                mCropImageView.setDrawable(bitmap, 200, 300);
+                mCropImageView.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_cut:
+                mCropImageView.setDrawable(mCropImageView.getCropImage(), 200, 300);
+                mCropImageView.setVisibility(View.VISIBLE);
+                break;
+            default:
+                break;
+        }
     }
 }
